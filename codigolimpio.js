23edd0cd70f1c8f389f39db5cc0cc743409342de
jess026p <67206641+jess026p@users.noreply.github.com>
2024-06-17 18:33:05 -0500

    CodigoLimpio

---
+++ codigolimpio.js
@@ -0,0 +1,15 @@
+let presentacion=["hola","yo","soy","jean"];
+let nombre="presentacion"[3];
+alert(nombre);
+let paises=["ec","pe","br"]
+let[pais1,pais2]=paises;
+alert (pais1);
+alert(pais2);
+
+let[pais3,pais4]=paises;
+alert(pais3);
+alert(pais4);
+//tercer caso
+let[pais5,pais6]=paises
+alert(pais5);
+alert(pais6);
